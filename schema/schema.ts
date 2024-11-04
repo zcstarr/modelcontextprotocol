04816651b46c053463df9b57b927eda051b5b0b8
Justin Spahr-Summers <justin@anthropic.com>
2024-11-04 13:33:10 +0000

    Use full list of syslog logging severities

--- schema/schema.ts
+++ schema/schema.ts
@@ -650,8 +650,11 @@ export interface LoggingMessageNotification extends Notification {
 
 /**
  * The severity of a log message.
+ * 
+ * These map to syslog message severities, as specified in RFC-5424:
+ * https://datatracker.ietf.org/doc/html/rfc5424#section-6.2.1
  */
-export type LoggingLevel = "debug" | "info" | "warning" | "error";
+export type LoggingLevel = "debug" | "info" | "notice" | "warning" | "error" | "critical" | "alert" | "emergency";
 
 /* Sampling */
 /**
