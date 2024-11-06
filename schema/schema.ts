6aaaaf9484392cf78cfa862273e049d8299489d2
Justin Spahr-Summers <justin@anthropic.com>
2024-11-06 11:54:15 +0000

    Remove duplicated comment

--- schema/schema.ts
+++ schema/schema.ts
@@ -853,14 +853,6 @@ export interface ModelPreferences {
 /**
  * Hints to use for model selection.
  *
- * - If the record contains a `name` key:
- *    - The client SHOULD treat this as a substring of a model name; for example:
- *        - `claude-3-5-sonnet` should match `claude-3-5-sonnet-20241022`
- *        - `sonnet` should match `claude-3-5-sonnet-20241022`, `claude-3-sonnet-20240229`, etc.
- *        - `claude` should match any Claude model
- *    - The client MAY also map the string to a different provider's model name or a different model family, as long as it fills a similar niche; for example:
- *        - `gemini-1.5-flash` could match `claude-3-haiku-20240307`
- *
  * Keys not declared here are currently left unspecified by the spec and are up
  * to the client to interpret.
  */
